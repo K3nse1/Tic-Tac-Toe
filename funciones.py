55d544b60c9cd5c5df7f4b13a69e2bf353a4bba9
K3nse1 <raulsangut999@gmail.com>
2023-11-03 17:32:00 +0100

    Tic-tac-toe game with NxN dimensions

---
+++ funciones.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+def user_turn(board):
+    row_position = int(input("Enter the row number: "))
+    column_position = int(input("Enter the column number: "))
+    board[row_position, column_position] = 1
+
+def cpu_turn(board, n):
+    random_row = np.random.randint(0, n)
+    random_column = np.random.randint(0, n)
+    
+    while board[random_row, random_column] != 0:
+        random_row = np.random.randint(0, n)
+        random_column = np.random.randint(0, n)
+        if 0 not in board:
+            print("Draw")
+            break
+            
+    board[random_row, random_column] = 2
+
+def row_winner(board):
+    for row in board:
+        if all(cell == 1 for cell in row) or all(cell == 2 for cell in row):
+            return True
+
+def column_winner(board, n):
+    for column in range(n):
+        column_values = board[:, column]
+        if all(cell == 1 for cell in column_values) or all(cell == 2 for cell in column_values):
+            return True
+
+def diagonal_winner(board):
+    counter = 0
+    diag = []
+
+    for row in range(len(board)):
+        diag.append(board[counter][-(counter+1)])
+        counter += 1
+
+    if all(cell == 1 for cell in np.diag(board)) or all(cell == 2 for cell in np.diag(board)) or all(cell == 1 for cell in diag) or all(cell == 2 for cell in diag):
+        return True
+
+def game(board, n):
+    while 0 in board:
+        print("User's Turn")
+        user_turn(board)
+        print(board)
+        if row_winner(board) or column_winner(board, n) or diagonal_winner(board):
+            print("We have a winner")
+            break
+        cpu_turn(board, n)
+        print("CPU's Turn")
+        print(board)
+        if row_winner(board) or column_winner(board, n) or diagonal_winner(board):
+            print("We have a winner")
+            break
--- main.py
+++ main.py
@@ -1,52 +1,12 @@
 import numpy as np
+from funciones import *
 
-n = int (input ("Introduce el número de filas que tendrá el tablero: "))
+n = int (input ("Enter the number of rows that the board will have: "))
 
-def determinar_ganador(tablero):
-
-    counter = 0
-    diag = []
-
-    for row in tablero:
-        diag.append(tablero[counter][-(counter+1)])
-        if all(cell == 1 or cell == 2 for cell in row) or all (cell == 1 or cell == 2 for cell in diag):
-            return True
-        counter += 1
-
-    for column in range(n):
-        column = tablero[:, column]
-        if all (cell == 1 or cell == 2 for cell in column):
-            return True
+while n <= 2:
+    n = int (input ("The number must be greater than two. Please enter another one:"))
         
-    
+board = np.zeros (shape=(n,n))
+print (board)
 
-tablero = np.zeros (shape=(n,n))
-print (tablero)
-
-while 0 in tablero:
-    
-    print ("Turno del usuario")
-    posicion_fila =  int ((input ("Introduce el número de fila: ")))
-    posicion_columna = int ((input ("Introduce el número de columna: ")))
-    tablero [posicion_fila, posicion_columna] = 1
-    print (tablero)
-    if determinar_ganador(tablero) == True:
-        print ("Tenemos un ganador")
-        break
-
-    fila_aleatoria = np.random.randint (0,n)
-    columna_aleatoria = np.random.randint (0,n)
-    
-    while tablero[fila_aleatoria, columna_aleatoria] != 0:
-        fila_aleatoria = np.random.randint (0,n)
-        columna_aleatoria = np.random.randint (0,n)
-        if 0 not in tablero:
-            print ("Empate")
-            break
-        
-    tablero [fila_aleatoria, columna_aleatoria] = 2
-    print ("Turno de la CPU")
-    print (tablero)
-    if determinar_ganador(tablero) == True:
-        print ("Tenemos un ganador")
-        break
+game (board, n)
